0413bb7c44e5bafa43bfcafb12ebb80227dc7a76
Skikbal <skikbalali02@gmail.com>
2025-04-23 08:15:09 +0530

    feat: implemnted file upload middleware with multer library

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extension);
+    const newName = `${basename}-${Date.now()}${extension}`;
+    cb(null, newName);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: {
+    fieldSize: 5 * 1024 * 1024, //5mb
+  },
+});
