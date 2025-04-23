943f3b4dfa751e90a1e2d797f94675fddf49e190
Skikbal <skikbalali02@gmail.com>
2025-04-23 12:08:58 +0530

    implemented imagekit service for storing the images

---
+++ server/src/services/imagekit.service.js
@@ -0,0 +1,30 @@
+import ImageKit from "imagekit";
+import fs from "fs";
+import {
+  IMAGEKIT_PUBLIC_KEY,
+  IMAGEKIT_PRIVATE_KEY,
+  IMAGEKIT_ENDPOINT_URL,
+} from "../config/envConfig.js";
+import ApiError from "../utils/ApiError.js";
+
+const imagekit = new ImageKit({
+  publicKey: IMAGEKIT_PUBLIC_KEY,
+  privateKey: IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: IMAGEKIT_ENDPOINT_URL,
+});
+
+const imagekitUpload = async (file) => {
+  try {
+    const image = await imagekit.upload({
+      file: file.path,
+      fileName: file.filename,
+    });
+    if (fs.existsSync(file.path)) fs.unlinkSync(file.path);
+    return image.url;
+  } catch (error) {
+    if (fs.existsSync(file.path)) fs.unlinkSync(file.path);
+    throw new ApiError(500, "Failed to upload image", error);
+  }
+};
+
+export default imagekitUpload;
