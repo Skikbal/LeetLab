32469f3eac829f9082c079a8d6da9195ba6d203a
Skikbal <skikbalali02@gmail.com>
2025-05-06 11:33:49 +0530

    feat(submission-controller): getAllsubmission  controller logic added

--- server/src/controllers/submission.controller.js
+++ server/src/controllers/submission.controller.js
@@ -1,11 +1,29 @@
-import AsyncHandler from "../utils/asyncHandler";
+import AsyncHandler from "../utils/asyncHandler.js";
+import { prisma } from "../libs/db.js";
+import ApiError from "../utils/ApiError.js";
+import ApiRespone from "../utils/ApiResponse.js";
 
-const getAllSubmissionsHandle = AsyncHandler(async (req, res) => {});
+const getAllSubmissionsHandler = AsyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const submissions = await prisma.Submission.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  if (!submissions) {
+    throw new ApiError(404, "Submissions not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiRespone(200, "Submissions fetched successfully", submissions));
+});
 const getSubmissionForProblemHandler = AsyncHandler(async (req, res) => {});
 const countSubmissionForProblemHandler = AsyncHandler(async (req, res) => {});
 
 export {
-  getAllSubmissionsHandle,
+  getAllSubmissionsHandler,
   getSubmissionForProblemHandler,
   countSubmissionForProblemHandler,
 };
