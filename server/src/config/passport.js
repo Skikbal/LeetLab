aac4255bc5f2116c81dc5395f86fe9449ea8fe33
Skikbal <skikbalali02@gmail.com>
2025-05-08 21:44:57 +0530

    feat(passport): Added github login passport config

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,6 +1,12 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./envConfig.js";
+import { Strategy as GitHubStrategy } from "passport-github2";
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+} from "./envConfig.js";
 
 passport.use(
   new GoogleStrategy(
@@ -9,11 +15,24 @@ passport.use(
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:8080/api/v1/auth/google/callback",
     },
-    function (accessToken, refreshToken, profile, cb) {
-      cb(null, {
+    function (accessToken, refreshToken, profile, done) {
+      done(null, {
         profile,
-        accessToken,
-        refreshToken,
+      });
+    },
+  ),
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/api/v1/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, {
+        profile,
       });
     },
   ),
